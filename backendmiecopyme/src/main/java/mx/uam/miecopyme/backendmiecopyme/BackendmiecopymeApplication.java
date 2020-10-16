676e363113aa90cf7386000d4c12610c9171aa93
CNRotceh <60460247+CNRotceh@users.noreply.github.com>
2020-10-16 10:45:11 -0500

    Commit inicial

---
+++ backendmiecopyme/src/main/java/mx/uam/miecopyme/backendmiecopyme/BackendmiecopymeApplication.java
@@ -0,0 +1,13 @@
+package mx.uam.miecopyme.backendmiecopyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendmiecopymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendmiecopymeApplication.class, args);
+	}
+
+}
---
+++ backendmiecopyme/src/test/java/mx/uam/miecopyme/backendmiecopyme/BackendmiecopymeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.miecopyme.backendmiecopyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendmiecopymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
