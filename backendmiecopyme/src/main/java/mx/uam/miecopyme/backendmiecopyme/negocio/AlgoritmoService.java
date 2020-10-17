9be25530c9b93beeac2d598298542602d079693b
CNRotceh <60460247+CNRotceh@users.noreply.github.com>
2020-10-17 18:22:57 -0500

    Error arreglado

--- backendmiecopyme/src/main/java/mx/uam/miecopyme/backendmiecopyme/negocio/AlgoritmoService.java
+++ backendmiecopyme/src/main/java/mx/uam/miecopyme/backendmiecopyme/negocio/AlgoritmoService.java
@@ -3,10 +3,22 @@ package mx.uam.miecopyme.backendmiecopyme.negocio;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
 import mx.uam.miecopyme.backendmiecopyme.negocio.modelo.Pyme;
 import mx.uam.miecopyme.backendmiecopyme.negocio.modelo.Servicio;
 import mx.uam.miecopyme.optimizahc.nsga2.NSGA2;
 
+/**
+ * 
+ * Clase que contiene la lógica de negocio del manejo de las Pymes
+ * 
+ * @author Prograsaur Studios
+ *
+ */
+@Service
+@Slf4j
 public class AlgoritmoService {
 	
 	public Iterable<Pyme> run(Pyme pymeActual){
